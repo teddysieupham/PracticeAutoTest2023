9e97b7c2e7e26131f9f11ef22080f146141d1527
Thạch Khánh Nhân <64407776+teddysieupham@users.noreply.github.com>
2023-11-06 18:32:15 +0700

    Update version 1.6

--- src/main/java/cucumberTest/TestRunnerHomePage.java
+++ src/main/java/cucumberTest/TestRunnerHomePage.java
@@ -7,11 +7,11 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/Feature", glue = { "src/main/stepDefinition" },
-		format = {"json:target/cucumber.json"},plugin = {
+		plugin = {
 		"html:target/cucumber-reports/TestRunnerHomePage.html",
 		"json:target/cucumber-reports/TestRunnerHomePage.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"pretty"
+		"pretty","json:target/cucumber.json"
 })
 public class TestRunnerHomePage {
 
